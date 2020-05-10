654748c4266fc1c30a92d2d330ae1edc185d4baf
Carlitos <bachilon97@gmail.com>
2020-05-10 13:53:22 +0200

    Subida de la primera funcion

---
+++ Funcion1.java
@@ -0,0 +1,23 @@
+package Funciones;
+
+public class Funcion1 {
+
+	public static Boolean Funcion1(String palabra) {
+
+	    int edad = 22;
+	    int cont = 0;
+	    Boolean result = null;
+	    for (int y = 0; y < palabra.length(); y++) {
+	         if ((palabra.charAt(y) == 'a') || ((palabra.charAt(y) == 'e')) || ((palabra.charAt(y) == 'o'))
+	             || ((palabra.charAt(y) == 'i')) || ((palabra.charAt(y) == 'u'))) {
+	                cont++;
+	                if (cont >= edad) {
+	                    result = true;
+	                } else {
+	                    result = false;
+	                }
+	            }
+	        }
+	   return result;
+	}
+}
