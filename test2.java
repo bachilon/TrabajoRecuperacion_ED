f47801dd09be6c89a916dfbe74c31530949828f1
bachilon <56442519+bachilon@users.noreply.github.com>
2020-05-10 14:16:08 +0200

    Eliminado

--- test2.java
+++ test2.java
@@ -1,5 +1,3 @@
-package Tests;
-
 import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,7 +34,7 @@ class test2 {
 		
 		@AfterAll
 		static void finPruebas() {
-			System.out.println("Pruebas realizadas con éxito");
+			System.out.println("Pruebas realizadas con Ã©xito");
 			funcion = null;
 		}
 		
